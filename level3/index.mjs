51ecc96a0c7ab52c668e60cf0e9fe6e8c6c367f1
Daniela BECERRA GONZALEZ <danybecerr@gmail.com>
2023-08-20 20:44:20 -0600

    Add function that needs a library

---
+++ level3/index.mjs
@@ -0,0 +1,12 @@
+import bcrypt from bcryptjs;
+
+export const handler = async (event) => {
+    const numSaltRounds = 8;
+    const password = event.password;
+    const hashedPassword = await bcrypt.hahsh(password, numSaltRounds);
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify("Hashed Password: " + hashedPassword),
+    };
+    return response;
+}
